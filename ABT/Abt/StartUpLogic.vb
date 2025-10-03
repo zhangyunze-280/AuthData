9a9a611fae7ec8d9fcc615a977c47781587e257d
fujisawakiyoshi <wf83455029@gmail.com>
2025-10-03 15:41:08 +0900

    認証データ要求Logic削除

--- ABT/Abt/StartUpLogic.vb
+++ ABT/Abt/StartUpLogic.vb
@@ -1,4 +1,5 @@
-﻿Public Class StartUpLogic
+﻿Option Strict On : Option Explicit On
+Public Class StartUpLogic
     Public Sub AbtOpenReceived(extra As String)
 
         Console.WriteLine($"[{DateTime.Now:HH:mm:ss}] StartUpLogic受信完了: " &
